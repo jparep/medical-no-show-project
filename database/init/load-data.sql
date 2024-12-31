997835b38b20bf211cd4e65b60ba0824d79e045c
jparep <parepson@gmail.com>
2024-12-30 17:38:26 -0700

    Updated docker compose

--- database/init/load-data.sql
+++ database/init/load-data.sql
@@ -1,17 +1,17 @@
 COPY appointments (
-    patient_id AS "PatientID",
-    appointment_id AS "AppointmentID",
-    gender AS "Gender",
-    scheduled_day AS "ScheduledDay",
-    appointment_day AS "AppointmentDay",
-    age AS "Age",
-    neighbourhood AS "Neighbourhood",
-    scholarship AS "Scholarship",
-    hypertension AS "Hypertension",
-    diabetes AS "Diabetes",
-    alcoholism AS "Alcoholism",
-    sms_received AS "SMSReceived",
-    no_show AS "NoShow"
+    patient_id,
+    appointment_id,
+    gender,
+    scheduled_day,
+    appointment_day,
+    age,
+    neighbourhood,
+    scholarship,
+    hypertension,
+    diabetes,
+    alcoholism,
+    sms_received,
+    no_show
 )
 FROM '/docker-entrypoint-initdb.d/processed_data.csv'
 DELIMITER ','
